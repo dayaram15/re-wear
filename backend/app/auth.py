fd8728e0fb719071ba52814aadd73e7df3a24689
yash2511106 <yashdhariyaparmar@gmail.com>
2025-07-12 14:40:41 +0530

    nn

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, set_access_cookies
 from app.models import User
 from app import db
 
